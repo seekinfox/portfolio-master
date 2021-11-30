7d961b00b8788900f95594fc94fdafb53a8aa752
seekinfox <sabhyankar918@gmail.com>
2021-12-01 00:33:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,68 @@
 (() => {
+    const item = document.querySelectorAll('.item-card');
     const menu = document.querySelector('.display-mobile');
     const icon = document.querySelector('.fas');
+    const popupIcon = document.querySelector('.contact');
+    const popup = document.querySelector('.contact-popup');
+    const slideBox = document.querySelector('.slider');
+    const buttons = document.querySelectorAll('.btn');
+    const eyes = document.querySelectorAll('.fa-eye');
+    const infoBox = document.querySelectorAll('.more');
+    const scroll = document.querySelectorAll('.scroll')
+    const form = document.getElementById('form')
+    const name = document.querySelector('#name')
+    const email = document.querySelector('#email')
+    const massage = document.querySelector('#msg')
+    const alertmsg = document.querySelector('.alert');
+
+    //for menu popup
     icon.addEventListener('click', () => {
         menu.classList.toggle('show');
     })
 
-    const popupIcon = document.querySelector('.contact');
-    const popup = document.querySelector('.contact-popup');
+
+    //for contact popup
     popupIcon.addEventListener('click', () => {
         popup.classList.add('block');
 
         const close = popup.querySelector('.close');
         close.addEventListener('click', () => {
-            popup.classList.remove('block');
+            popup.classList.remove('block')
         })
     })
 
-    const slideBox = document.querySelector('.slider');
-    const buttons = document.querySelectorAll('.btn');
+
+    //for filter and some buggy animation
     buttons.forEach((btn) => {
+        //filter
         btn.addEventListener('click', (e) => {
             e.preventDefault();
+            const filter = e.target.dataset.filter;
+
+            item.forEach((item) => {
+                const itemFilter = item.dataset.item;
+                if (filter == 'all') {
+                    console.log('all')
+                    item.style.display = 'inherit';
+                } else if (filter == itemFilter) {
+                    item.style.display = 'block'
+
+                } else {
+
+                    item.style.display = 'none'
+
+                }
+
+            })
+
+            //red slider on filter navigation
             if (e.target.classList.contains('all')) {
-                console.log('returned')
                 slideBox.classList.add('activep1')
             } else {
                 slideBox.classList.remove('activep1')
             }
             if (e.target.classList.contains('hc')) {
-                console.log('returned')
+
                 slideBox.classList.add('activep2')
             } else {
                 slideBox.classList.remove('activep2')
@@ -44,9 +78,8 @@
     })
 
 
-    const eyes = document.querySelectorAll('.fa-eye');
-    const infoBox = document.querySelectorAll('.more');
 
+    //for item menu eye
     eyes.forEach((eye) => {
 
         eye.addEventListener('click', (e) => {
@@ -56,8 +89,12 @@
                 if (box.classList.contains(eyeData)) {
                     box.classList.add('block')
                     eye.parentElement.previousElementSibling.previousElementSibling.addEventListener('click', () => {
+                        box.classList.add('none');
+                        setTimeout(() => {
+                            box.classList.remove('block');
+                            box.classList.remove('none');
 
-                        box.classList.remove('block');
+                        }, 300)
                     })
                 }
             })
@@ -66,10 +103,10 @@
         })
     })
 
-    const scroll = document.querySelectorAll('.scroll')
 
-    const item = document.querySelectorAll('.item-card');
 
+
+    //for website preview slider
     scroll.forEach((item) => {
         item.addEventListener('click', (e) => {
             const eye = e.target.parentElement.nextElementSibling.firstElementChild.dataset.eye;
@@ -84,6 +121,39 @@
 
     })
 
+    //send email on submit
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const msg = '<br><b>Name:</b>' + name.value + '<br><b>Email:</b>' + email.value + '<br><br>' + massage.value;
+        console.log(msg)
 
+
+        Email.send({
+            //token is generated from elastic-email.com
+
+            SecureToken: "5906bf1d-4f65-4200-bdf6-530b11610051",
+            To: 'sabhyankar918@gmail.com',
+            From: email.value,
+            Subject: "New message from portfolio",
+            Body: msg
+        }).then(
+            message => {
+                console.log(massage.value)
+                if (message) {
+
+                    //feedback massage :)
+                    alertmsg.classList.add('block')
+                    setTimeout(() => {
+                        alertmsg.classList.remove('block');
+                    }, 7000)
+                }
+            }
+        );
+        //empty the input content after the email submit
+        name.value = '';
+        email.value = '';
+        massage.value = '';
+
+    })
 
 })()
